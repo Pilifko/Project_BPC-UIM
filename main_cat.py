6b3ee08b57e8260480349c52c13a15dd8fc0537f
dalkoo <159259138+docenasek@users.noreply.github.com>
2025-11-19 21:53:05 +0100

    catboost

--- main_cat.py
+++ main_cat.py
@@ -1,26 +1,14 @@
 from catboost import CatBoostClassifier
-from pandas.io.xml import preprocess_data
 from sklearn.model_selection import StratifiedKFold, cross_val_predict
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
-import numpy as np
-import pandas as pd
 from main import *
-from main import load_data
 
 X, y = data_preprocessing(load_data("heart-disease_data.csv"))
 
 
 kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
 
-model = CatBoostClassifier(
-    iterations=400,
-    learning_rate=0.03,
-    depth=3,
-    loss_function='Logloss',
-    eval_metric='F1',
-    verbose=False,
-    random_state=39,
-)
+model = CatBoostClassifier(random_strength=2, learning_rate=0.01, l2_leaf_reg=7, iterations=1000, depth=4, border_count=32, verbose=False)
 
 y_pred = cross_val_predict(model, X, y, cv=kfold)
 
